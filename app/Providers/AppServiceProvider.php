bc191b2fd161e3d17196c940d1523cd3f2f6cf73
Surtr <cherryred5959@gmail.com>
2021-02-19 03:54:17 +0900

    reformatting AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,7 @@ namespace App\Providers;
 use App\Http\Controllers\ListenBotManController;
 use BotMan\BotMan\BotMan;
 use Illuminate\Notifications\AnonymousNotifiable;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
-use Psr\Log\LoggerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,15 +18,19 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->when(ListenBotManController::class)
             ->needs(AnonymousNotifiable::class)
-            ->give(function () {
-                return (new AnonymousNotifiable())->route('slack', config('logging.channels.slack.url'));
-            });
+            ->give(
+                function () {
+                    return (new AnonymousNotifiable())->route('slack', config('logging.channels.slack.url'));
+                }
+            );
 
         $this->app->when(ListenBotManController::class)
             ->needs(BotMan::class)
-            ->give(function () {
-                return $this->app['botman'];
-            });
+            ->give(
+                function () {
+                    return $this->app['botman'];
+                }
+            );
     }
 
     /**
